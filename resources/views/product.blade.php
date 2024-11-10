1390c038ff68938517d5b0d8a063fe06a23c96c7
bimajm7 <bimajm7bima@apps.ipb.ac.id>
2024-11-10 16:59:41 +0700

    fixing php file

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -138,11 +138,7 @@
 
     <!-- BREADCRUMB AREA START -->
     <div class="ltn__breadcrumb-area ltn__breadcrumb-area-2 ltn__breadcrumb-color-white bg-overlay-theme-black-90 bg-image" data-bs-bg="img/IMG_1229.jpg">
-
-        <div class="container">
-
         <div class="container-fluid">
-
             <div class="row">
                 <div class="col-lg-12">
                     <div class="ltn__breadcrumb-inner ltn__breadcrumb-inner-2 justify-content-between">
@@ -164,11 +160,7 @@
     
     <!-- PRODUCT DETAILS AREA START -->
     <div class="ltn__product-area ltn__product-gutter mb-120">
-
-        <div class="container">
-
         <div class="container-fluid">
-
             <div class="row">
                 <div class="col-lg-12">
                     <div class="tab-content">
@@ -279,11 +271,7 @@
 <!-- FOOTER AREA START (ltn__footer-2 ltn__footer-color-1) -->
     <footer class="ltn__footer-area ltn__footer-2 ltn__footer-color-1">
         <div class="footer-top-area  section-bg-2">
-
-            <div class="container">
-
             <div class="container-fluid">
-
                 <div class="row">
                     <div class="col-xl-4 col-md-5">
                         <div class="footer-widget ltn__footer-timeline-widget ltn__footer-timeline-widget-1 bg-image bg-overlay-theme-black-90">
@@ -337,11 +325,7 @@
             </div>
         </div>
         <div class="ltn__copyright-area ltn__copyright-2">
-
-            <div class="container">
-
             <div class="container-fluid">
-
                 <div class="row">
                     <div class="col-md-6 col-12">
                         <div class="site-logo-wrap">
